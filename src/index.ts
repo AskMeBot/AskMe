b29c755b3624fb72fd4ea9179a361705973b7b26
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-07-04 17:56:48 -0700

    Update index.ts
    - Updated select placeholder text
    - Added dev commands

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,160 @@ client.on("interaction", (interaction) => {
 
     //Trivia commands/events
     let triviaQuestions:TriviaConfig = JSON.parse(readFileSync("./trivia.json").toString())
+
+    //Dev trivia (Debug)
+    if(interaction.isCommand() && interaction.commandName == "devtrivia"){
+        let footerContent = []
+        const currentQuestionIndex = (interaction.options.first().value as number)
+        const currentQuestion = triviaQuestions[currentQuestionIndex]
+        if(!currentQuestion)
+            return interaction.reply({embeds:[{"title":"😬 Uh oh, this is awkward", "description":`This server doesn't appear to have any trivia questions available. Try again later, or if you believe this is a mistake, contact the bot support server.`}], components:[{"type":1,"components":[{"type":2,"label":"Try again","style":"SECONDARY","customID":`play_again_dev`}]}], ephemeral:true}).catch(console.error)
+        if(currentQuestion.guild_id && currentQuestion.guild_id == interaction.guildID)
+            footerContent.push(`ℹ️ This is a server-specific question`)
+        if(currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array))
+            footerContent.push(`#️⃣ You need to select ${currentQuestion.answer.length} options`)
+        return interaction.reply({embeds:[{
+            "title":"Trivia",
+            "description":`${currentQuestion.question}`,
+            "footer":{
+                "text":footerContent.join(" | ")
+            }
+        }],
+        components:[{
+            "type":"ACTION_ROW",
+            "components":[{
+                "type":"SELECT_MENU",
+                "customID":"select_answer_dev",
+                "placeholder":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?`Select ${currentQuestion.answer.length} answers`:`Select your answer`,
+                "options":currentQuestion.choices.map(ta => {
+                    return {"label":ta.title, "value":`answer_${currentQuestionIndex}//${ta.optionName}//${interaction.member?interaction.member.user.id:interaction.user.id}`}
+                }),
+                "minValues":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?currentQuestion.answer.length:undefined,
+                "maxValues":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?currentQuestion.answer.length:undefined
+            }]
+        },
+        {
+            "type":1,
+            "components":[
+                {
+                    "type":2,
+                    "label":"Skip",
+                    "style":"DANGER",
+                    "customID":`play_again_dev`
+                }
+            ]
+        }],
+        ephemeral:false
+        })
+        .catch(console.error)
+    }
+
+    //Dev selected answer
+    if(interaction.isSelectMenu() && interaction.customID == "select_answer_dev"){
+        const selectedOptions = interaction.values
+        if(!selectedOptions[0].startsWith("answer_"))
+            return interaction.reply({"content":"Invalid interaction."})
+        const currentQuestion = triviaQuestions[(selectedOptions[0].split("//")[0].replace("answer_", "") as any as number)]
+        let isAnswerCorrect = false
+
+        //Multiple selections
+        if(currentQuestion.answer instanceof Array && currentQuestion.requireAllAnswersSelected && selectedOptions.filter(v => currentQuestion.answer.includes(v.split("//")[1])).length == currentQuestion.answer.length)
+            isAnswerCorrect = true;
+
+        //Multiple answers, one selection
+        if(currentQuestion.answer instanceof Array && !currentQuestion.requireAllAnswersSelected && currentQuestion.answer.includes(selectedOptions[0].split("//")[1]))
+            isAnswerCorrect = true;
+        
+        if(typeof currentQuestion.answer == "string" && !currentQuestion.requireAllAnswersSelected && currentQuestion.answer == selectedOptions[0].split("//")[1])
+            isAnswerCorrect = true;
+        //currentQuestion.answer == selectedOption.split("//")[1]?`✅`:`❌`
+        let newEmbed = new MessageEmbed()
+        .setTitle("Trivia")
+        .setDescription(currentQuestion.question)
+        .addFields([
+            {
+                "name":"Values",
+                "value":`[${selectedOptions.join(", ")}]`
+            },
+            {
+                "name":"Correct",
+                "value":`${currentQuestion.answer}`
+            }
+        ])
+        .setFooter(`${isAnswerCorrect?`✅`:`❌`} ${isAnswerCorrect?`CORRECT`:`INCORRECT`}`);
+        interaction.update({
+            components:[
+                {
+                    "type":1,
+                    "components":[
+                        {
+                            "type":2,
+                            "label":"Continue",
+                            "style":"PRIMARY",
+                            "customID":`play_again_dev`
+                        }
+                    ]
+                }
+            ],
+            embeds:[newEmbed]
+        })
+        .catch(console.error)
+    }
+    if(interaction.isButton() && interaction.customID == "play_again_dev"){
+        let footerContent = [];
+        const currentQuestionIndex = Math.floor(Math.random() * triviaQuestions.length)
+        const currentQuestion = triviaQuestions[currentQuestionIndex]
+        if(!currentQuestion)
+            return interaction.update({embeds:[{"title":"😬 Uh oh, this is awkward", "description":`This server doesn't appear to have any trivia questions available. Try again later, or if you believe this is a mistake, contact the bot support server.`}], components:[{"type":1,"components":[{"type":2,"label":"Try again","style":"SECONDARY","customID":`play_again_dev`}]}]}).catch(console.error)
+        if(currentQuestion.guild_id && currentQuestion.guild_id == interaction.guildID)
+            footerContent.push(`ℹ️ This is a server-specific question`)
+        if(currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array))
+            footerContent.push(`#️⃣ You need to select ${currentQuestion.answer.length} options`)
+        interaction.update({embeds:[
+            {
+                "title":"Trivia",
+                "description":`${currentQuestion.question}`,
+                footer:{
+                    "text":footerContent.join(" | ")
+                }
+            }
+        ], components:[
+            {
+                "type":"ACTION_ROW",
+                "components":[
+                    {
+                        "type":"SELECT_MENU",
+                        "customID":"select_answer_dev",
+                        "placeholder":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?`Select ${currentQuestion.answer.length} answers`:`Select your answer`,
+                        "options":currentQuestion.choices.map(ta => {
+                            return {"label":ta.title, "value":`answer_${currentQuestionIndex}//${ta.optionName}//${interaction.member?interaction.member.user.id:interaction.user.id}`}
+                        }),
+                        "minValues":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?currentQuestion.answer.length:undefined,
+                        "maxValues":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?currentQuestion.answer.length:undefined
+                    }
+                ]
+            },
+            {
+                "type":1,
+                "components":[
+                    {
+                        "type":2,
+                        "label":"Skip",
+                        "style":"DANGER",
+                        "customID":`play_again_dev`
+                    }
+                ]
+            }
+        ]})
+        .catch(console.error)
+    }
+
+    //Normal usage
     triviaQuestions = triviaQuestions.filter(tq => {
         return tq && (!tq.guild_id || tq.guild_id == interaction.guildID)
     });
+
+
     if(interaction.isCommand() && interaction.commandName == "trivia"){
         let footerContent = []
         const currentQuestionIndex = Math.floor(Math.random() * triviaQuestions.length)
@@ -46,6 +197,7 @@ client.on("interaction", (interaction) => {
             "components":[{
                 "type":"SELECT_MENU",
                 "customID":"select_answer",
+                "placeholder":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?`Select ${currentQuestion.answer.length} answers`:`Select your answer`,
                 "options":currentQuestion.choices.map(ta => {
                     return {"label":ta.title, "value":`answer_${currentQuestionIndex}//${ta.optionName}//${interaction.member?interaction.member.user.id:interaction.user.id}`}
                 }),
@@ -68,6 +220,7 @@ client.on("interaction", (interaction) => {
         })
         .catch(console.error)
     }
+
     if(interaction.isSelectMenu() && interaction.customID == "select_answer"){
         const selectedOptions = interaction.values
         if(!selectedOptions[0].startsWith("answer_"))
@@ -108,6 +261,7 @@ client.on("interaction", (interaction) => {
         })
         .catch(console.error)
     }
+
     if(interaction.isButton() && interaction.customID == "play_again"){
         let footerContent = [];
         const currentQuestionIndex = Math.floor(Math.random() * triviaQuestions.length)
@@ -133,6 +287,7 @@ client.on("interaction", (interaction) => {
                     {
                         "type":"SELECT_MENU",
                         "customID":"select_answer",
+                        "placeholder":currentQuestion.requireAllAnswersSelected && (currentQuestion.answer instanceof Array)?`Select ${currentQuestion.answer.length} answers`:`Select your answer`,
                         "options":currentQuestion.choices.map(ta => {
                             return {"label":ta.title, "value":`answer_${currentQuestionIndex}//${ta.optionName}//${interaction.member?interaction.member.user.id:interaction.user.id}`}
                         }),
